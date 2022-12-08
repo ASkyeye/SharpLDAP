17bd4493376c6d5368aac5adbc88974ea0e2abcf
Mert Daş <48562581+mertdas@users.noreply.github.com>
2022-12-08 22:24:00 +0300

    Add files via upload

---
+++ SharpLDAP/Help.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLDAP.Enum
+{
+    public class Help
+    {
+        public static void Help1()
+        {
+
+            Console.WriteLine("\nUsage:\n\n Enumerate Users: SharpLDAP.exe dcname.local Users \n\n Enumerate Computers: SharpLDAP.exe dcname.local Computers");
+        }
+    }
+}
